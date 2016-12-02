9fad0776f18d1b065346ad78fe374320abcd5f19
Roman Sadoyan <core01@bk.ru>
2016-12-03 02:34:17 +0300

    Добавлен обработчик 404 страниц

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,20 @@
+@extends('base')
+@section('title')404 - Page Not Found
+@endsection
+@section('content')
+    <section class="hero is-fullheight is-cover-image">
+        <div class="hero-body">
+            <div class="container">
+                <div class="columns is-vcentered">
+                    <div class="column is-4 is-offset-4">
+                        <div id="main">
+                            <div class="content">
+                            <h1>Oops! Page Not Found!</h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
