b38bb9ddb96e5194f65c39454a1a883db56d87b8
Roman Sadoyan <core01@bk.ru>
2016-12-03 02:35:05 +0300

    Добавлен favicon, изменен background блока

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title') | Backend developer</title>
+    <link rel="shortcut icon" href="{{asset('favicon.png')}}" type="image/png">
     <link rel="stylesheet" href="{{asset('assets/css/app.css')}}" type="text/css">
     <link rel="stylesheet" href="{{asset('assets/font-awesome/css/font-awesome.min.css')}}" type="text/css">
     @yield('styles')
